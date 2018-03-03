108961841587cb4ee4c0bced503655dfcdf78d33
gvc2610 <chaitanyaieee@gmail.com>
2018-03-02 22:52:45 -0800

    updated hello-world.c

--- hello-world.c
+++ hello-world.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+
+int add(int a,int b) {
+	return a+b;
+}
+int sub(int a,int b) {
+	return a-b;
+}
+
 int main()
 {
 	/* code */
 	printf("Hello World\n");
+    
+    int (*funptr)(int,int);
+    funptr = add;
+    printf("funptr:%d  add:%d\n",funptr(2,3),add(2,3));
+
+
+
 	return 0;
 }
