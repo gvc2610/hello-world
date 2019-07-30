9f190e2cfca3b97bf01bbd89e986b758377f286f
gvc2610 <chaitanyaieee@gmail.com>
2019-07-30 03:58:19 -0700

    64to32bit.c

---
+++ 64to32bit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+
+ uint8_t x1, x2, y1, y2, z1, z2; 
+/* set x1, x2, y1, y2 here */ 
+
+x2 = 0x02, x1 = 0x80; // MSB,LSB
+y2=  0x04, y1 = 0x80; 
+
+z1 = z2 = 0; 
+
+z1 = x1 + y1; 
+if (z1 < x1 || z1 < y1) { 
+/* overflow */ 
+z2++; 
+} 
+z2 += x2 + y2; 
+
+printf("z2:%x z1:%x \n",z2,z1 );
+
+	return 0;
+}
